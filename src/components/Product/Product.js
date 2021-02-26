755b1a28d69a5a3c24a0dbea4b2edef9953d7ecd
vitorlc <vitor_cirqueira1@hotmail.com>
2021-02-26 07:53:55 -0300

    Fix product list

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,24 +2,33 @@ import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
 import { AddShoppingCart } from '@material-ui/icons'
 
-function Product({ product, handleClick }) {
+function Product({ product, handleClick, isCart }) {
   return (
-    <Card style={{ margin: 10 }}>
+    <Card style={{ margin: 10, height: 400 }}>
       <CardMedia image="" title={product.name} />
-      <img src={product.picture} width="200" height="200"></img>
+      <img src={product.picture} width="200" height="150"></img>
       <CardContent>
-        <Typography variant="h5">
+        <Typography variant="body1">
           {product.title}
         </Typography>
-        <Typography variant="h5">
-          R$ {product.price}
-        </Typography>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="Adicione ao Carrinho" onClick={() => handleClick(product)}>
-          <AddShoppingCart />
-        </IconButton>
-      </CardActions>
+      {
+        isCart ? 
+          <CardActions disableSpacing style={{ marginTop: 'auto' }}>
+            <Typography variant="h6">
+              R$ {product.price}
+            </Typography>
+          </CardActions>
+          :
+          <CardActions disableSpacing style={{ marginTop: 'auto' }}>
+            <Typography variant="h6">
+              R$ {product.price}
+            </Typography>
+            <IconButton aria-label="Adicione ao Carrinho" onClick={() => handleClick(product)} style={{ marginLeft: 'auto' }}>
+              <AddShoppingCart />
+            </IconButton>
+          </CardActions>
+      }
     </Card>
   )
 }
--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 import Product from '../Product/Product'
 
-const ProductsList = ({products, handleClick}) => {
+const ProductsList = ({products, handleClick, isCart}) => {
   return (
-    <main>
+    <main style={{ marginTop: 70 }}>
       <Grid container justify="center" spacing="{4}">
         {products.map((product)=> (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={2}>
-            <Product product={product} handleClick={handleClick}/>
+            <Product product={product} handleClick={handleClick} isCart={isCart}/>
           </Grid>
         ))}
       </Grid>
