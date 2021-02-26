59b6f2b4b370249e1c6aa62c710f9d485f9da805
vitorlc <vitor_cirqueira1@hotmail.com>
2021-02-26 07:54:35 -0300

    Add Cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { useSelector } from "react-redux"
+import { Typography, ButtonBase } from '@material-ui/core'
 
 import { ProductsList, Navbar } from '../components'
 
 const Cart = () => {
   const store = useSelector(state => state.cartState)
+
+  const calculateTotal = (products) => {
+    return products.reduce((accum, curr) => accum + curr.price , 0)
+  }
+
   return (
     <div>
-      <Navbar/>
-      <ProductsList products={store.products}></ProductsList>
+      <Navbar isCart={true} />
+      <ProductsList products={store.products} isCart={true}></ProductsList>
+      <div style={{}}>
+        <Typography variant="h6">Total: {`R$ ${calculateTotal(store.products)}`}</Typography>
+        <ButtonBase style={{}}>
+          <span style={{}}>Finalizar Compra</span>
+        </ButtonBase>
+      </div>
     </div>
   )
 }
