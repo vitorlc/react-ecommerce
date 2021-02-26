f15a010197672f438d5ef12a8186b4f940ff2f3f
vitorlc <vitor_cirqueira1@hotmail.com>
2021-02-26 07:53:25 -0300

    Add Navbar on Cart

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core'
-import { ShoppingCart } from '@material-ui/icons'
+import { ShoppingCart, KeyboardBackspace } from '@material-ui/icons'
 import { useHistory } from 'react-router-dom'
 
-const Navbar = ({ cartCount }) => {
+const Navbar = ({ cartCount, isCart }) => {
   let history = useHistory()
   return (
     <>
       <AppBar position="fixed" >
         <Toolbar>
           <Typography>REACT E-COMMERCE</Typography>
-          <IconButton onClick={() => history.push("/carrinho")} aria-label="Carrinho" style={{ position: 'absolute', right: 0 }}>
-            <Badge badgeContent={cartCount} color="secondary">
-              <ShoppingCart />
-            </Badge>
-          </IconButton>
+          {isCart ? 
+            <IconButton onClick={() => history.push("/")} aria-label="Voltar" style={{ position: 'absolute', right: 0 }}>
+              <KeyboardBackspace />
+            </IconButton>
+            :
+            <IconButton onClick={() => history.push("/carrinho")} aria-label="Carrinho" style={{ position: 'absolute', right: 0 }}>
+              <Badge badgeContent={cartCount} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          }
         </Toolbar>
       </AppBar>
     </>
