e84b7bdfdd867609517207f3e9bf56c6a757b01a
vitorlc <vitor_cirqueira1@hotmail.com>
2021-03-03 18:44:51 -0300

    Add filter duplicate in cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector } from "react-redux"
 import { Typography, ButtonBase } from '@material-ui/core'
 
@@ -6,17 +6,39 @@ import { ProductsList, Navbar } from '../components'
 
 const Cart = () => {
   const store = useSelector(state => state.cartState)
+  const [cart, setCart] = useState([])
+  const [cartCost, setCartCost] = useState(0)
 
-  const calculateTotal = (products) => {
-    return products.reduce((accum, curr) => accum + curr.price , 0)
-  }
+  useEffect(() => {
+    setCart([])
+    setCartCost(0)
+    if (store.products.length) {
+      let cost = 0
+      let cartArray = store.products.reduce( (acc, cur) => {
+        let found = acc.find(e=> e.id === cur.id)
+        if(found) {
+          found.count +=1
+        } else {
+          cur.count = 1
+          acc.push(cur)
+        } 
+        return acc
+      }, [])
+      
+      for (let item of cartArray) {
+        cost += (item.price * item.count)
+        setCart(prevArray => [...prevArray, item])
+      }
+      setCartCost(cost)
+    }
+  }, [store.products])
 
   return (
     <div>
       <Navbar isCart={true} />
-      <ProductsList products={store.products} isCart={true}></ProductsList>
+      <ProductsList products={cart} isCart={true}></ProductsList>
       <div style={{}}>
-        <Typography variant="h6">Total: {`R$ ${calculateTotal(store.products)}`}</Typography>
+        <Typography variant="h6">Total: {`R$ ${cartCost}`}</Typography>
         <ButtonBase style={{}}>
           <span style={{}}>Finalizar Compra</span>
         </ButtonBase>
