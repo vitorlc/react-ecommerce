40b0575b82d02282931f5c3d3084351cd3527b31
vitorlc <vitor_cirqueira1@hotmail.com>
2021-03-03 19:38:20 -0300

    Add remove item from store redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 import { ADD_ITEM } from './actionTypes'
+import { REMOVE_ITEM } from './actionTypes'
 
 export const addItemCart = value => ({
   type: ADD_ITEM,
   item: value
-}); 
+})
+
+export const removeItemCart = value => ({
+  type: REMOVE_ITEM,
+  item: value
+})
--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,4 +1,5 @@
 import { ADD_ITEM } from '../actions/actionTypes'
+import { REMOVE_ITEM } from '../actions/actionTypes'
 
 const initialState = {
   products: []
@@ -8,6 +9,15 @@ export const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_ITEM: 
       return {...state, products: [...state.products, action.item]}
+    case REMOVE_ITEM: 
+      return {...state, products: state.products.reduce((acc, cur) => {
+          if (cur.id == action.item.id && cur.count !== 1) {
+            cur.count = cur.count - 1
+            acc.push(cur)
+          } 
+          return acc
+        }, [])
+      }
     default: 
       return state
   }
