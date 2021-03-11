9f12d97d16ca305133b6270f607756a55e65e697
vitorlc <vitor_cirqueira1@hotmail.com>
2021-03-11 20:01:43 -0300

    Add product list component styles

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 import Product from '../Product/Product'
+import useStyles from './styles'
 
 const ProductsList = ({ products, handleClick, isCart, handleDecrement, handleIncrement }) => {
+  const classes = useStyles()
   return (
-    <main style={{ marginTop: 70 }}>
-      <Grid container justify="center" spacing="{4}">
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <Grid container justify="center" spacing={4}>
         {products.map((product) => (
-          <Grid item key={product.id} xs={12} sm={6} md={4} lg={2}>
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
             <Product product={product} handleClick={handleClick} isCart={isCart} handleDecrement={handleDecrement} handleIncrement={handleIncrement} />
           </Grid>
         ))}
